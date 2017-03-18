777227c42e8e182db6a4fba68c24652e816e2832
yu123105 <fishruijie@163.com>
2017-03-18 08:42:45 +0800

    服务器端搭建完成

--- proxy-web/src/main/java/com/fish/proxy/web/ProxyController.java
+++ proxy-web/src/main/java/com/fish/proxy/web/ProxyController.java
@@ -1,13 +1,12 @@
 package com.fish.proxy.web;
 
 
-import com.fasterxml.jackson.core.JsonFactory;
+import com.alibaba.fastjson.JSON;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Enumeration;
@@ -17,29 +16,27 @@ import java.util.Map;
 @Controller
 public class ProxyController {
 
-    @RequestMapping("/")
+    @RequestMapping("/message")
     @ResponseBody
     public String proxyVerify(HttpServletRequest request){
         Enumeration<String> names = request.getHeaderNames();
         Map<String, String> heads = new HashMap<>();
         Map<String, String[]> params = request.getParameterMap();
-        ObjectMapper objectMapper = new ObjectMapper();
-
-
         while (names.hasMoreElements()){
             String name = names.nextElement();
             String value = request.getHeader(name);
             heads.put(name, value);
         }
-        try {
-            String headStr = objectMapper.writeValueAsString(heads);
-            String paramStr = objectMapper.writeValueAsString(params);
-            return compositeHtml(headStr, paramStr);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
+        heads.put("remote-address", request.getRemoteAddr());
 
-        return null;
+        String headStr = JSON.toJSONString(heads);
+        String paramStr = JSON.toJSONString(params);
+        return compositeHtml(headStr, paramStr);
+    }
+    @RequestMapping("real-ip")
+    @ResponseBody
+    public String realIp(HttpServletRequest request){
+        return request.getRemoteAddr();
     }
     private String compositeHtml(String heads, String params){
         String html = "<html>";
